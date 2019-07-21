0fd19aba4046c07cb52bbe4754c523883860d35e
t.ferreria@live.ca <t.ferreira@live.ca>
2019-07-21 01:12:20 -0400

    wrapped up mainpage and started experience section on secondary page

--- new/main.js
+++ new/main.js
@@ -1,33 +1,4 @@
- 
-/*
- function load() {
- 	var iters = 2;
- 	for (i = 0; i < iters; i++) {
- 		setTimeout(function(){
- 			document.getElementById("one").style.display = "block";
-		 	document.getElementById("two").style.display = "none";
-		 	document.getElementById("three").style.display = "none";
- 		}, 20000);
-	 	setTimeout(function(){}, 1000);
-	 	document.getElementById("one").style.display = "none";
-	 	document.getElementById("two").style.display = "block";
-	 	document.getElementById("three").style.display = "none"
-	 	setTimeout(function(){}, 1000);
-	 	document.getElementById("one").style.display = "none";
-	 	document.getElementById("two").style.display = "none";
-	 	document.getElementById("three").style.display = "block";
- 	}
-
- 	document.getElementById("load-screen").style.display = "none";
-	document.getElementById("main").style.display = "block";
-	document.getElementById("projects").style.display = "block";
-
-
- }
- */
-
-
-/* Open */
+ /* Open */
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
   document.getElementById("projects").style.display = "none";
