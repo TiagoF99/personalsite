d205bf46b5d37720a012179d0efad95b2ae094bd
t.ferreria@live.ca <t.ferreira@live.ca>
2019-08-28 15:55:52 -0400

    removed secondary page and added to main, updated resume and project content

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,11 @@
  /* Open */
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
-  document.getElementById("projects").style.display = "none";
-
 }
 
 /* Close */
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
-  document.getElementById("projects").style.display = "block";
 }
 
 function moreinfoclk() {
