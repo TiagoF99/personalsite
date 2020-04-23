4be5d427477859e5d2251b49147e84c553c8fce8
t.ferreria@live.ca <t.ferreira@live.ca>
2020-04-22 22:46:13 -0400

    updating 2020-04-22

--- assets/onlcoursecomponent.js
+++ assets/onlcoursecomponent.js
@@ -5,8 +5,9 @@ var app4 = new Vue({
     {title: 'IBM Data Science Professional Certificate' , comp: 'IBM | Coursera.com ', date: 'Issued Jul 2019 - Aug 2019'},
     {title: 'AWS Cloud Practitioner Essentials (Second Edition): AWS Core Services' , comp: 'AWS', date: 'Issued Jun 2019'},
     {title: 'Building a Full-Stack App with Angular 2+ and Spring Boot, DevOps Foundations' , comp: 'Lynda.com', date: 'Issued Jun 2019'},
+    {title: 'Android Developer Fundamentals Course', comp: 'Google Developers', date: 'Issued April 2019'},
     {title: 'HTML, CSS, and JavaScript for Web Developers' , comp: 'The Johns Hopkins University | Coursera.com', date: 'Issued Dec 2018'},
-    {title: 'Using Python to Access Web Data', comp: 'University of Michigan | Coursera.com', date: 'Issued Jul 2019'}
+    {title: 'Using Python to Access Web Data', comp: 'University of Michigan | Coursera.com', date: 'Issued December 2018'}
     ]
   }
 })
